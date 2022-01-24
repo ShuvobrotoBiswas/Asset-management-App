3bcd6db336e1ad046dccb850053cbe212e2444c1
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-01-24 16:14:57 +0600

    Update Edit-Equipments.vue

--- src/pages/Edit-Equipments.vue
+++ src/pages/Edit-Equipments.vue
@@ -1,12 +1,10 @@
 <template>
    <div class="q-pa-md q-gutter-md" style="max-width: 2400px">
-     <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md"
+     <q-form @submit.prevent="editProducts"
     >
      <!-- Inputs -->
-     <q-select v-model="model" :options="options" label="CATEGORY" />
+      
+     <q-select v-model="category_name" :options="displayProduct" label="CATEGORY" option-label="name" />
      <!-- Date -->
       <q-input  v-model="date" mask="date" :rules="['date']" label="DATE ADDED">
       <template v-slot:append>
@@ -22,31 +20,39 @@
       </template>
      </q-input>
      <!-- Date End -->
-     <q-input v-model="text" label="BRAND" />
-     <q-input v-model="text" label="MODEL" />
-     <q-input v-model="text" label="DESCRIPTION" />
+     <q-input v-model="category_id" label="CATEGORY ID" />
+     <q-input v-model="brand_name" label="BRAND" />
+     <q-input v-model="model" label="MODEL" />
+     <q-input v-model="description" label="DESCRIPTION" />
 
      <!-- Checkbox -->
      <div>
       <q-checkbox
-        v-model="customModel"
+        v-model="defect_details"
+        color="blue"
+        label="Mark Defected"
+        true-value="yes"
+        false-value="no"
+      />
+      
+     <q-input v-model="text" label="REASON FOR DEFECTING" />
+     </div>
+     <div>
+      <q-checkbox
+        v-model="discard_details"
         color="blue"
         label="Mark Discarded"
         true-value="yes"
         false-value="no"
       />
-     <q-input v-model="text" label="REASON FOR DISCARDING" />
-    <q-option-group
-      :options="option1"
-      type="checkbox"
-      v-model="group"
-    />
+      
      <q-input v-model="text" label="REASON FOR DISCARDING" />
      </div>
+    
      <!-- Checkbox End -->
      <q-card-actions align="right" class="text-primary">
-        <q-btn label="Cancel" type="reset" color="primary" flat class="q-ml-sm" />
-         <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn to="/Equipments" label="Cancel" type="reset" color="primary" flat class="q-ml-sm" />
+         <q-btn  label="Submit" type="submit" color="primary"/>
         </q-card-actions>
      </q-form>
    </div>
@@ -55,28 +61,79 @@
 <script>
 import { ref } from 'vue'
 import { useQuasar } from 'quasar'
+import axios from "axios";
+
 export default {
   // name: 'PageName',
-  setup () {
+  data () {
     const text = ref(null)
      const $q = useQuasar()
 
     return {
-      group: ref([]),
-      text,
-      model: ref(null),
-      options: [
-        'LAPTOP', 'MOUSE', 'KEYBOARD', 'HEADPHONE', 'PC'
-      ],
-       option1: [
-        { label: 'Mark Discarded', value: 'bat' },
-      ],
-      date: ref('2022/01/01'),
-      customModel: ref(''),
-      save: ref(''),
-      onSubmit () {''},
-      onReset () {text.value = null}
+      group: " ",
+      category_id: "",
+      brand_name: "",
+      category_name: " ",
+      model: " ",
+      text: "",
+      description: "" ,
+      displayProduct: [],
+      name: " ",
+      attributes: [],
+      
+      date: "",
+      defect_details: "",
+      discard_details: "",
+      save: "",
+     
     }
-  }
+  },
+
+      // SHOW
+
+  created (){
+    const options = {
+  method: 'GET',
+  url: 'http://127.0.0.1:8000/api/categories/',
+  headers: { "Authorization": "Bearer 2|CowcsAIjzegopghpUBJu0cjv3dMeQtFdS9Z2Ovfl"}
+};
+
+axios.request(options).then( ( response) =>{
+  console.log(response.data);
+  this.displayProduct=response.data
+}).catch(function (error) {
+  console.error(error);
+});
+
+  },
+// Edit
+  methods: {   
+editProducts (){
+   const attributes = new FormData();
+  attributes.append( "product_code", this.product_code);
+  attributes.append( "category_id",  this.category_id);
+  attributes.append( "brand",  this.brand);
+  attributes.append( "date",  this.date);
+  attributes.append( "model",  this.model);
+  attributes.append( "description",  this.description);
+  attributes.append( "defect_details",  this.defect_details);
+  attributes.append( "discard_details",  this.discard_details);
+    const options = {
+  method: 'PATCH',
+  url: 'http://127.0.0.1:8000/api/products/5',
+  data: {
+    name:this.attributes
+  },
+  headers: { Authorization: "Bearer 2|CowcsAIjzegopghpUBJu0cjv3dMeQtFdS9Z2Ovfl" } 
+};
+ axios.request(options).then((response) => {
+   console.log(response.data);
+   this.attributes = response.data;
+ })
+ .catch(function (error) { console.error(error) ;
+ } );
+
+},
+}
 }
 </script>
