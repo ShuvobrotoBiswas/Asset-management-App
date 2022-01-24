03c3a1afcc8b13aaa906a1e7d96deb2a688536d7
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-01-24 16:18:10 +0600

    Update Users.vue

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
+        <q-toolbar>
+          <q-toolbar-title>
+            USERS
+          </q-toolbar-title>
+        </q-toolbar>
+      
     <q-list v-if="listUser.length" bordered separator class="text-weight-bolder">
-      <q-item to="/User-Equipments" clickable v-ripple
+      <q-item :to="`/User-Equipments/${user.id}`" clickable v-ripple
         v-for="user in listUser"
         :key="user.uid"
       >
@@ -11,9 +17,12 @@
             <img :src=" 'http://127.0.0.1:8000\\uploads\\users\\'+ user.image" alt="" />
           </q-avatar>
         </q-item-section>
-        <q-item-section>{{ user.full_name }} {{ user.image}}</q-item-section>
+        <q-item-section>{{ user.full_name }} </q-item-section>
       </q-item>
     </q-list>
+
+    <!-- Add Users -->
+    
     <div class="q-pa-md q-gutter-sm fixed-bottom-right " >
     <q-btn
       @click="prompt = true"
@@ -27,27 +36,45 @@
     <q-dialog v-model="prompt" persistent>
       <q-card style="min-width: 350px">
         <q-card-section>
-          <q-form>
-            <q-input v-model="save" type="text" label="First Name" />
-            <q-input type="text" label="Last Name" />
-            <q-card-actions align="right">
-              <q-btn class="q-mt-xl" color="gray" text-color="#ddd"
+      <q-form @submit.prevent="addUsers"  class="q-gutter-md">
+      <q-input
+        name="first_name"
+        v-model="first_name"
+        color="primary"
+        label="First Name"
+      />
+       <q-input
+        name="last_name"
+        v-model="last_name"
+        color="primary"
+        label="Last Name"
+      />
+      <q-input
+        @update:model-value="val => { image = val[0] }"
+        filled
+        type="file"
+        hint="Native file"
+      />
+
+      <div>
+        <q-btn class="q-mt-xl" color="gray" text-color="#ddd"
                 unelevated
                 label="Cancel"
                 no-caps
                 v-close-popup
               />
-              <q-btn
+         <q-btn
                 class="q-mt-xl"
                 color="blue"
                 text-color="white"
                 unelevated
                 label="Save"
+                type="submit"
                 no-caps
                 v-close-popup
-              />
-            </q-card-actions>
-          </q-form>
+              />     
+      </div>
+    </q-form>
         </q-card-section>
       </q-card>
     </q-dialog>
@@ -59,17 +86,21 @@
 <script>
 import axios from "axios";
 
-
 export default {
   // name: 'PageName',
    data()  {
     return {
+      first_name: '  ',
+      last_name: ' ',
+      image: '',
+      full_name: ' ',
       prompt: false,
       listUser:[],
       address: "",
+     
     };
   },
-
+// SHOW
   created (){
     const options = {
   method: 'GET',
@@ -83,11 +114,38 @@ axios.request(options).then( ( response) =>{
 }).catch(function (error) {
   console.error(error);
 });
+
   },
+  // ADD
+methods: {   
+onFileSelected(event){
+console.log(event.target);
+},
+
+addUsers (){
+  const user = new FormData();
+  user.append( "first_name", this.first_name);
+  user.append( "last_name",  this.last_name);
+  user.append( "image",  this.image);
   
+    const options = {
+  method: 'POST',
+  url: 'http://127.0.0.1:8000/api/users',
+  data: user,
+
+  headers: { Authorization: "Bearer 2|CowcsAIjzegopghpUBJu0cjv3dMeQtFdS9Z2Ovfl" } 
+};
+ axios.request(options).then((response) => {
+   console.log(response.data);
+   this.user = response.data;
+ })
+ .catch(function (error) { console.error(error) ;
+ } );
+
+},
+
+},
+
 
 }
 </script>
-<style>
-
-</style>
