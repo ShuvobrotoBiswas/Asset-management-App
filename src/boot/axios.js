cc6d7e8c475387e0c929458bc119e515d02f7bfb
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-02-01 15:52:14 +0600

    Update axios.js

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,13 +1,16 @@
 import { boot } from 'quasar/wrappers'
+// import Vue from "vue"
 import axios from 'axios'
-
+// Vue.prototype.$axios = axios
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
 // If any client changes this (global) instance, it might be a
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' })
+const api = axios.create({ baseURL: 'https://assets-api.dev.sandbox3000.com/api' ,
+headers:{'Authorization' : 'Bearer ' + localStorage.getItem("token")}
+})
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
